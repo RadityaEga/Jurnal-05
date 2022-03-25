ecdad1b98cb45e7687a73cc9740f872586421e96
Raditya Ega <89286716+RadityaEga@users.noreply.github.com>
2022-03-26 03:08:54 +0700

    Update Soal02.java

--- List/Soal02.java
+++ List/Soal02.java
@@ -3,50 +3,57 @@ import java.util.*;
     public class Soal02 {
         public static void main(String[] args) {
             Queue todoList = new Queue();
-        Scanner input = new Scanner(System.in);
-        int pilihan;
-        do {
-            System.out.println("TODO LIST WITH QUEUE ");
-            System.out.println("1. Tambahkan Todo");
-            System.out.println("2. Tampllkan Todo yang harus dikerjakan Terlebih dahulu");
-            System.out.println("3. Hapus TODO yang sudah selesai");
-            System.out.println("4. Tampllkan Semua Todo LIST ");
-            System.out.println("5. keluar");
+            Scanner input = new Scanner(System.in);
+            int pilihan;
 
-            pilihan = input.nextInt();
+            do {
+                System.out.println("Pilih Menu ");
+                System.out.println("1. Menambah nama pekerjaan yang harus dilakukan");
+                System.out.println("2. Menampilkan nama pekerjaan yang harus diselesaikan terlebih dahulu");
+                System.out.println("3. Menghapus pekerjaan yang sudah diselesaikan");
+                System.out.println("4. Menampilkan seluruh isi to-do list");
+                System.out.println("5. keluar");
 
-            if (pilihan == 1) {
-                System.out.println("Masukan todo");
-                String todo = input.next();
-                todoList.insert(todo);
+                pilihan = input.nextInt();
+
+                if (pilihan == 1) {
+                    System.out.println("Masukan nama Pekerjaan");
+                    String todo = input.next();
+                    todoList.insert(todo);
+                }
+
+                else if (pilihan == 2) {
+                    if (todoList.isQueueEmpty()) {
+                        System.out.println("Pekerjaan kosong harap tambahkan pekerjaan!");
+                    }
+                    else {
+                        System.out.println("Pekerjaan yang harus segera diselesaikan");
+                        String todoAwal = todoList.peek();
+                        System.out.println("Pekerjaan pertama " + todoAwal);
+                    }
+                }
+
+                else if (pilihan == 3) {
+                    if (todoList.isQueueEmpty()) {
+                        System.out.println("Pekerjaan Kosong harap tambahkan Pekerjaan");
+                    }
+
+                    else {
+                        todoList.delete();
+                    }
+                }
+
+                else if (pilihan == 4) {
+                    if (todoList.isQueueEmpty()) {
+                        System.out.println("Pekerjaan Kosong harap tambahkan Pekerjaan");
+                    }
+
+                    else {
+                        todoList.display();
+                    }
+                }
             }
-            else if (pilihan == 2) {
-                if (todoList.isQueueEmpty()) {
-                    System.out.println("Todo Kosong harap tambahkan Todo");
-                }
-                else {
-                    System.out.println("Todolist yang harus segera diselesaikan");
-                    String todoAwal = todoList.peek();
-                    System.out.println("Todo pertama " + todoAwal);
-                }
-            }
-            else if (pilihan == 3) {
-                if (todoList.isQueueEmpty()) {
-                    System.out.println("Todo Kosong harap tambahkan Todo");
-                }
-                else {
-                    todoList.delete();
-                }
-            }
-            else if (pilihan == 4) {
-                if (todoList.isQueueEmpty()) {
-                    System.out.println("Todo Kosong harap tambahkan Todo");
-                }
-                else {
-                    todoList.display();
-                }
-            }
+                while (pilihan < 5);
         }
-            while (pilihan < 5);
     }
-}
+
