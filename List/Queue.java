c8555ee2eeeb4fcd9c1f96b90592fb217073ec27
Raditya Ega <89286716+RadityaEga@users.noreply.github.com>
2022-03-26 03:09:56 +0700

    Update Queue.java

--- List/Queue.java
+++ List/Queue.java
@@ -1,32 +1,29 @@
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.NoSuchElementException;
-import java.util.Queue;
-
-    public class Queue {
-       private LinkList Todo;
+ public class Queue {
+        private LinkList Todo;
         public Queue() {
-            Todo=new LinkList();
-        }
-        
-        public void insert(String data) {
-            Todo.tambahkanData(data);
-            System.out.println("Todo Berhasil Ditambahkan");
-        }
-        
-        public String delete() {
-            return Todo.hapusData();
-        }
-        
-        public boolean isQueueEmpty() {
-            return Todo.isEmpty();
-        }
-        
-        public void display() {
-            Todo.displayList();
-        }
-        
-        public String peek() {
-            return Todo.peekFirst();
-        }
+        Todo=new LinkList();
     }
+
+    public void insert(String data) {
+        Todo.tambahkanData(data);
+        System.out.println("Pekerjaan Berhasil Ditambahkan");
+    }
+
+    public String delete() {
+        return Todo.hapusData();
+    }
+
+    public boolean isQueueEmpty() {
+        return Todo.isEmpty();
+    }
+
+    public void display() {
+        Todo.displayList();
+    }
+
+    public String peek() {
+        return Todo.peekFirst();
+    }
+}
+
+
