8e66c0da1da8f7e2016756e53587cb651a265a8c
Raditya Ega <89286716+RadityaEga@users.noreply.github.com>
2022-03-26 03:10:41 +0700

    Update Node.java

--- List/Node.java
+++ List/Node.java
@@ -1,32 +1,15 @@
 import java.util.*;
 
     public class Node {
-        private String data;
-        private Node link;
+        String data;
+        Node next;
 
-        public Node() {
-            link = null;
-            data = "Kosong";
+        public Node(String data) {
+            this.data = data;
         }
 
-        public Node(String a,Node b) {
-            data = a;
-            link = b;
-        }
-
-        public void setLink(Node b) {
-            link = b;
-        }
-
-        public void setData(String a) {
-            data = a;
-        }
-
-        public Node getLink() {
-            return link;
-        }
-
-        public String getData() {
-            return data;
+        public void displayNode()
+        {
+            System.out.println(data);
         }
     }
