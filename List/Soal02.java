0e1b3fedf62117d508aea82055e2820bdcf54d3a
Raditya Ega <89286716+RadityaEga@users.noreply.github.com>
2022-03-26 02:51:05 +0700

    Update Soal02.java

--- List/Soal02.java
+++ List/Soal02.java
@@ -2,30 +2,51 @@ import java.util.*;
 
     public class Soal02 {
         public static void main(String[] args) {
-            QueueA<String> queue = new QueueA<>();
-            Scanner in = new Scanner(System.in);
-            int opsi = 0;
-            while(opsi != 4){
-                System.out.println("1. Masukkan Data");
-                System.out.println("2. Menghapus Data");
-                System.out.println("3. Print list");
-                System.out.println("4. Keluar");
-                opsi = in.nextInt();
-                if(opsi == 1){
-                    System.out.println("Silahkan Masukkan Data");
-                    String toDo = in.next();
-                    queue.enqueue(toDo);
-                    System.out.println();
+            Queue todoList = new Queue();
+        Scanner input = new Scanner(System.in);
+        int pilihan;
+        do {
+            System.out.println("TODO LIST WITH QUEUE ");
+            System.out.println("1. Tambahkan Todo");
+            System.out.println("2. Tampllkan Todo yang harus dikerjakan Terlebih dahulu");
+            System.out.println("3. Hapus TODO yang sudah selesai");
+            System.out.println("4. Tampllkan Semua Todo LIST ");
+            System.out.println("5. keluar");
+
+            pilihan = input.nextInt();
+
+            if (pilihan == 1) {
+                System.out.println("Masukan todo");
+                String todo = input.next();
+                todoList.insert(todo);
+            }
+            else if (pilihan == 2) {
+                if (todoList.isQueueEmpty()) {
+                    System.out.println("Todo Kosong harap tambahkan Todo");
                 }
-                else if(opsi == 2){
-                    queue.dequeue();
-                    System.out.println("Berhasil Menghapus Tugas Pertama");
-                    System.out.println();
+                else {
+                    System.out.println("Todolist yang harus segera diselesaikan");
+                    String todoAwal = todoList.peek();
+                    System.out.println("Todo pertama " + todoAwal);
                 }
-                else if(opsi == 3){
-                    queue.printQueue();
-                    System.out.println();
+            }
+            else if (pilihan == 3) {
+                if (todoList.isQueueEmpty()) {
+                    System.out.println("Todo Kosong harap tambahkan Todo");
+                }
+                else {
+                    todoList.delete();
+                }
+            }
+            else if (pilihan == 4) {
+                if (todoList.isQueueEmpty()) {
+                    System.out.println("Todo Kosong harap tambahkan Todo");
+                }
+                else {
+                    todoList.display();
                 }
             }
         }
+            while (pilihan < 5);
     }
+}
